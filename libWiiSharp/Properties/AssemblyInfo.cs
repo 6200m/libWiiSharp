053b5a43fd07dad3f87fa77df06185cee21baf13
Andreas Bielawski <brawl98@outlook.com>
2019-05-12 18:00:35 +0200

    Update version number to v0.31

--- libWiiSharp/Properties/AssemblyInfo.cs
+++ libWiiSharp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyTitle("libWiiSharp")]
 [assembly: AssemblyDescription("a wii related .NET library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Leathl")]
+[assembly: AssemblyCompany("WiiDatabase.de, Leathl")]
 [assembly: AssemblyProduct("libWiiSharp")]
 [assembly: AssemblyCopyright("Copyright © Leathl 2010")]
 [assembly: AssemblyTrademark("")]
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
